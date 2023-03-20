c350a7682654a24f5882891e0128176c1fdd7694
川俣 州生 <s_kawamata@IT-PC-2001-0418.local>
2023-03-20 22:28:12 +0900

    commit

--- scripts/kintai_check_over_45_hours.py
+++ scripts/kintai_check_over_45_hours.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 import time
 from selenium.webdriver.support.ui import Select
-#from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
 import user_info
 import user_list
 from selenium.webdriver import DesiredCapabilities
