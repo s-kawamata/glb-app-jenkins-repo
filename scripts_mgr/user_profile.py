d566c220ffb0d1c2b1d21722c30e83684a6fc1b8
川俣 州生 <s_kawamata@IT-PC-2001-0418.local>
2023-04-10 15:40:25 +0900

    add

--- scripts_mgr/user_profile.py
+++ scripts_mgr/user_profile.py
@@ -1,3 +1,3 @@
-#shusei_kawamata = "user_profile/yckwb8hz.default-release/"
-shusei_kawamata =  "/Users/s_kawamata/Library/Application Support/Firefox/Profiles/yckwb8hz.default-release/"
+shusei_kawamata = "user_profile/yckwb8hz.default-release/"
+#shusei_kawamata =  "/Users/s_kawamata/Library/Application Support/Firefox/Profiles/yckwb8hz.default-release/"
 goolge_pw = "19921107Wanko"
